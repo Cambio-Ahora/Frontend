0dc91bc9569f5e6edfecbb5a909d20fa29d87e43
José Puma <45923235+josepuma@users.noreply.github.com>
2020-01-05 11:41:38 -0300

    First commit ever

---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+
+export default {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'uikit/dist/css/uikit.css',
+    './assets/main.scss',
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [{ src: '~/plugins/uikit', ssr: false }],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/style-resources',
+    ['prismic-nuxt', {
+      endpoint: 'https://sayrin-website.cdn.prismic.io/api/v2',
+      linkResolver: function(doc, ctx) {
+        return '/'
+      }
+    }]
+  ],
+  styleResources: {
+    scss: [
+      './assets/variables.scss',
+      ]
+  },
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  
+  div
+    p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+</template>
+
+<script>
+
+
+export default {
+  components: {
+    
+  }
+}
+</script>
