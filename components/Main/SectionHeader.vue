2d5c226123ab316afe0bbab62e3686bbec6a7b46
José Puma <45923235+josepuma@users.noreply.github.com>
2020-03-04 10:11:14 -0300

    Update SectionHeader.vue

--- components/Main/SectionHeader.vue
+++ components/Main/SectionHeader.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 
-    section.uk-section(v-view.once="isInView", :style="{ 'background-color' : data.primary.color_de_fondo }")
+    section.uk-section.uk-section-small(v-view.once="isInView", :style="{ 'background-color' : data.primary.color_de_fondo }")
         div.uk-container
             div.uk-text-center(ref="animateImage")
                 h2.uk-margin-remove.animate-image {{ $prismic.asText(data.primary.encabezado) }}
