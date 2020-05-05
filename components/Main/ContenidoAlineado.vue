7680da65838ab31bab8564cfbe118789ad47e6fa
José Puma <45923235+josepuma@users.noreply.github.com>
2020-05-05 11:06:32 -0400

    Update ContenidoAlineado.vue

--- components/Main/ContenidoAlineado.vue
+++ components/Main/ContenidoAlineado.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
     .uk-section(v-if="data", :class="{ 'uk-text-left': data.primary.alineamiento == 'izquierda' ,'uk-text-center': data.primary.alineamiento == 'centrado', 'uk-text-right': data.primary.alineamiento == 'derecha' }")
-        div(class="uk-width-3-5@m uk-width-1-1 uk-margin-auto")
-            h2 {{ $prismic.asText(data.primary.titulo) }}
-            .uk-margin
-                div(v-html="$prismic.asHtml(data.primary.contenido)")
+        .uk-container
+            div(class="uk-width-3-5@m uk-width-1-1 uk-margin-auto")
+                h2 {{ $prismic.asText(data.primary.titulo) }}
+                .uk-margin
+                    div(v-html="$prismic.asHtml(data.primary.contenido)")
 </template>
 
 <script>
